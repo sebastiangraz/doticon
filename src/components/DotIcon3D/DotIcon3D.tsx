1978a5017422441d7c985e4994ef60b73bed3396
Sebastian Graz <graz@live.se>
2026-03-30 18:40:23 +0200

    setup new files

---
+++ src/components/DotIcon3D/DotIcon3D.tsx
@@ -0,0 +1,9 @@
+const DotIcon3D = () => {
+  return (
+    <div>
+      <p>DotIcon3D</p>
+    </div>
+  );
+};
+
+export default DotIcon3D;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import DotIcon from "../components/DotIcon/DotIcon";
-
+import DotIcon3D from "../components/DotIcon3D/DotIcon3D";
 export const Route = createFileRoute("/")({
   component: () => (
     <>
       <DotIcon />
+      <h1>DotIcon3D</h1>
+      <DotIcon3D />
     </>
   ),
 });
